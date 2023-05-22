3447e506b8aab9925d36cb1d954c45f8586c651a
Alberto Nieto <anietor23@gmail.com>
2023-05-22 19:08:50 +0200

    Clase any

--- Algoritmes/src/es/iesmz/ed/algoritmes/Any.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Any.java
@@ -1,12 +1,27 @@
 package es.iesmz.ed.algoritmes;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Any {
-    private long any;
+
+    private final long any;
 
     public Any(long any) {
         this.any = any;
     }
-    public int  digitsDiferents(){
+    public int digitsDiferents() {
+        List<Character> digitsList = new ArrayList<>();
+        String anyoStr = String.valueOf(any);
 
+        for (char digitChar : anyoStr.toCharArray()) {
+            if (!digitsList.contains(digitChar)) {
+                digitsList.add(digitChar);
+            }
+        }
+        return digitsList.size();
     }
 }
+
