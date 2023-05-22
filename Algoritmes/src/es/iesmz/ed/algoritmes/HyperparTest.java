045005145b6f1a28387e3c44482ea417c3a5d70a
Alberto Nieto <anietor23@gmail.com>
2023-05-22 18:26:23 +0200

    Se ha añadido con la documentación.

---
+++ Algoritmes/src/es/iesmz/ed/algoritmes/HyperparTest.java
@@ -0,0 +1,39 @@
+package es.iesmz.ed.algoritmes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyperparTest {
+
+    @Test
+    public void test1() {
+        Hyperpar hyper = new Hyperpar(266846);
+        assertTrue(hyper.esHyperPar());
+    }
+    @Test
+    public void test2() {
+        Hyperpar hyper = new Hyperpar(166876);
+        assertFalse(hyper.esHyperPar());
+    }
+    @Test
+    public void test3() {
+        Hyperpar hyper = new Hyperpar(6822468);
+        assertTrue(hyper.esHyperPar());
+    }
+    @Test
+    public void test4() {
+        Hyperpar hyper = new Hyperpar(8466482);
+        assertTrue(hyper.esHyperPar());
+    }
+    @Test
+    public void test5() {
+        Hyperpar hyper = new Hyperpar(1266845);
+        assertFalse(hyper.esHyperPar());
+    }
+    @Test
+    public void test6() {
+        Hyperpar hyper = new Hyperpar(68448822);
+        assertTrue(hyper.esHyperPar());
+    }
+}
