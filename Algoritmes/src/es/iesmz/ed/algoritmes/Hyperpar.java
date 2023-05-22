bdc4dc4e21bf2d5159f8985e189114b3e20cf8d3
Alberto Nieto <anietor23@gmail.com>
2023-05-22 18:24:25 +0200

    Se ha añadido con la documentación.

--- Algoritmes/src/es/iesmz/ed/algoritmes/Hyperpar.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Hyperpar.java
@@ -1,12 +1,27 @@
 package es.iesmz.ed.algoritmes;
+/**
+ * Esta clase compara los numeros y si son todos los numeros introducidos pares para saber si son hyperPares
+ * y si hay algún número impar no son hyperPares
+ * @author: Alberto Nieto
+ * @version: 22/05/2023
+ */
+
 
 public class Hyperpar {
     private long numero;
+    /**
+     * Constructor para el número hyperPar
+     * @param numero El parámetro define un número que se va a comprobar.
+     */
 
     public Hyperpar(long numero) {
         this.numero = numero;
     }
 
+    /**
+     * Método que compara si es par o impar
+     * @return true o false dependiendo si es par o impar. Par (true), impar (false)
+     */
     public boolean esHyperPar() {
         String num = String.valueOf(numero);
         int contadorPar = 0;
