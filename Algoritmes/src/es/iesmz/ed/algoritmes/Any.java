096b1b1f7126076a70fbfdd74cf95568e2905eb6
Alberto Nieto <anietor23@gmail.com>
2023-05-22 19:15:06 +0200

    Clase Con documentación

--- Algoritmes/src/es/iesmz/ed/algoritmes/Any.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Any.java
@@ -1,5 +1,9 @@
 package es.iesmz.ed.algoritmes;
-
+/**
+ * Esta clase cuenta los digitos diferentes en el número any.
+ * @author: Alberto Nieto
+ * @version: 22/05/2023
+ */
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,10 +11,17 @@ import java.util.List;
 public class Any {
 
     private final long any;
-
+    /**
+     * Constructor para los digitos any
+     * @param any El parámetro define un número que se va a contar.
+     */
     public Any(long any) {
         this.any = any;
     }
+    /**
+     * Método que cuenta los numeros.
+     * @return los numeros diferentes del numero introducido
+     */
     public int digitsDiferents() {
         List<Character> digitsList = new ArrayList<>();
         String anyoStr = String.valueOf(any);
